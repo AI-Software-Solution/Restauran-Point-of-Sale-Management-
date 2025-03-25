52f17d1e0a176745f41752798af2374d42b7ada6
Azamat-JS <azamatielts987@gmail.com>
2025-03-25 16:05:45 +0500

    token verification added

--- pos-backend/routes/userRoute.js
+++ pos-backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getUserData, logout, updateUserData, deleteUserData, getUsers } = require("../controllers/userController");
+const { register, login, getUserData, logout, updateUserData, deleteUserData, getUsers, checkToken } = require("../controllers/userController");
 const { isVerifiedUser } = require("../middlewares/tokenVerification");
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
 // Authentication Routes
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/checkToken").put(checkToken);
 router.route("/logout").post(isVerifiedUser, logout)
 router.route("/update").put(isVerifiedUser, updateUserData)
 router.route("/delete").delete(isVerifiedUser, deleteUserData)
